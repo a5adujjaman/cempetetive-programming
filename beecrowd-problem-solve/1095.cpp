d2a58abce609c341c5dfce0fd56e688c35627bd3
a5adujjaman <asadujjamanasif46567@gmail.com>
2024-09-16 11:08:23 +0600

    first commit

--- beecrowd-problem-solve/1095.cpp
+++ beecrowd-problem-solve/1095.cpp
@@ -1,12 +1,14 @@
 #include<bits/stdc++.h>
-using namespace std; 
+using namespace std;
 int main(){
-int i; 
-int j=-2; 
+int i;
+int j=-2;
 for( i=60; i>=0; i-=5){
             j+=3;
             cout<<"I="<<j;
         cout<<" J="<<i<<endl;
 }
 return 0;
+
+//everything okay
 }
